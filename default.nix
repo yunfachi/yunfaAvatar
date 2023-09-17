d04f7ce41496bc6d2036e9302fd0dae1410de6c8
yunfachi <yunfachi@gmail.com>
2023-09-17 14:07:03 +0000

    nix

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
-{ lib, stdenvNoCC, fetchFromGitHub, bash, makeWrapper, pciutils
-, x11Support ? true, ueberzug, fetchpatch
+{ lib
+, stdenvNoCC
+, fetchFromGitHub
+, bash
+, makeWrapper
+, pciutils
+, x11Support ? true
+, ueberzug
+, fetchpatch
 }:
 stdenvNoCC.mkDerivation rec {
   pname = "yunfaavatar";
@@ -11,7 +18,7 @@ stdenvNoCC.mkDerivation rec {
     rev = "bf8ea66e981712e5201aeea49fa005836408f45f";
     sha256 = "sha256-JbolO1I4K9+RBFJ/U611En4Rx5cG2+z1IcxYEeiaq6Q=";
   };
-  
+
   outputs = [ "out" "man" ];
 
   strictDeps = true;
@@ -25,7 +32,7 @@ stdenvNoCC.mkDerivation rec {
 
   meta = with lib; {
     description = "Utility for automatic centralized changing of avatar in Github, Discord, Steam, Hypixel, etc. ";
-    homepage = "https://github.com/yunfachi/yunfaAvatar"; 
+    homepage = "https://github.com/yunfachi/yunfaAvatar";
     license = licenses.unfree;
     platforms = platforms.all;
     maintainers = with maintainers; [ yunfachi ];
